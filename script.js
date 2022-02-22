09e9b0ed0e31d6539633c3d2165065682198d6dd
rahel seyum <rachiellseyum@gmail.com>
2022-02-21 17:51:09 -0800

    Getting data from database.

--- script.js
+++ script.js
@@ -1,61 +1,69 @@
 // To add a todo list
 
 function addItem(event) {
-    event.preventDefault();
+    event.preventDefault(); 
     let text = document.getElementById("todo-input");
+    console.log(text.value);
     db.collection("todo-items").add({
         text: text.value,
         status: "active"
     })
+ 
     text.value = "";
 }
 
-// to display the todo lists
+//to display the todo lists
 function getItem() {
     db.collection("todo-items").onSnapshot((snapshot) => {
-        console.log(snapshot);
+         let items=[];
         snapshot.docs.forEach((doc) => {
-            items.push(doc.data)({
-                id: doc.id,
+            items.push({
+                id: doc.id, 
                 ...doc.data()
             })
-        } )
+            console.log(items);
+        })
         generateItems(items);
     })
 }
-// function generateItems(items) {
-//     let itmsHTML = "";
-//     items.forEach((item) => {
-//         itemsHTML += `
-//         <div class="todo-item">
-//         <div class="check">
-//             <div data-id="${item.id} class="check-mark">
-//                 <img src="./assets/icon-check.svg" alt="">
 
-//             </div>
+function generateItems(items) {
+    let itemsHTML = "";
+    items.forEach((item) => {
+        itemsHTML += `
+        <div class="todo-item">
+        <div class="check">
+            <div data-id="${item.id} class="check-mark">
+                <img src="./assets/icon-check.svg" alt="">
 
-//         </div>
-//         <div class="todo-text">
-//         ${items.text}
-//         </div>
-//     </div>
-//  `
-// })
-// document.querySelector("todo-items").innerHTML=itemsHTML;
-// }
-// function createEventListeners(){
-//     let todoCheckMarks=document.querySelectorAll(".todo-item .check-mark")
-//     todoCcheckMarks.forEach((checkMark)=>{
-//     checkMark.addEventListener("click", function(){
-//         markCompeted(checkMark.dataset.id);
+            </div>
 
-//     })
-// })
-// }
+        </div>
+        <div class="todo-text">
+        ${item.text}
+        </div>
+    </div>
+ `
+})
+document.querySelector(".todo-items").innerHTML=itemsHTML;
+createEventListeners();
+}
+function createEventListeners(){
+    let todoCheckMarks=document.querySelectorAll(".todo-item .check-mark");
+    // todoCheckMarks.forEach((checkMark)=>{
+    // checkMark.addEventListener("click", function(){
+    //     markCompeted(checkMark.dataset.id);
+
+    //})
+//})
+console.log(todoCheckMarks);
+}
+
+
 // function markCompeted(){
 //     db.collection("todo-items").doc()
 //     console
 // }
-// getItems();
+  getItem();
 
 
